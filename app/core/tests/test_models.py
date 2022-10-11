6acced63d4d038c4410281f98661e880f50a82b7
bg <oochanbg@gmail.com>
2022-10-11 09:23:42 +0630

    long time no see

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,6 +8,11 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
+def create_user(email="example@mail.com", password="example1234"):
+    """Create and return user"""
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTests(TestCase):
     """Test models."""
 
@@ -58,11 +63,17 @@ class ModelTests(TestCase):
             "test@example.com",
             "testpass123")
         recipe = models.Recipe.objects.create(
-            user = user,
-            title = "recipe title",
-            time_minutes = 5,
-            description = "recip descriptions",
-            price = Decimal('5.50'))
+            user=user,
+            title="recipe title",
+            time_minutes=5,
+            description="recip descriptions",
+            price=Decimal('5.50'))
 
-        self.assertEqual(str(recipe),recipe.title)
+        self.assertEqual(str(recipe), recipe.title)
 
+    def test_create_tag(self):
+        """Create test"""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user,name="Tag1")
+
+        self.assertEqual(str(tag),tag.name)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+"""
+View for the recipe APIs
+"""
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from core.models import Recipe, Tag
+from recipe import serializers
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """View for manage recipe APIs"""
+
+    serializer_class = serializers.RecipeDetailSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve recipes for authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        """return recipe serializers"""
+
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe"""
+        serializer.save(user=self.request.user)
+
+
+class TagViewSet(
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+):
+
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user).order_by('-name')
